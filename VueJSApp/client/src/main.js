e3d94f6a5b451daf477678ed377d7fa94989342f
Black Man <blackman@mailinator.com>
2021-09-03 22:51:33 +0300

    Setup of VueJs-Vuetify template

--- VueJSApp/client/src/main.js
+++ VueJSApp/client/src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+//axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+//axios
+Vue.use(VueAxios, axios)
+
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
